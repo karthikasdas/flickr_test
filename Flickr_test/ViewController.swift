b17dd55b18f388cdb3822e2686bfb53359dc63e5
karthikasdas <karthika.radha@gmail.com>
2021-06-04 16:55:33 +0400

    Initial Commit

---
+++ Flickr_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flickr_test
+//
+//  Created by Karthika on 6/4/21.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
